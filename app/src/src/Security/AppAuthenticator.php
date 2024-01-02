931b663f0a9566c96133e09e6bd02ff64c1b9559
karimi <m.karimi.sil@gmail.com>
2024-01-02 06:46:47 -0800

    add form and design admin

--- app/src/src/Security/AppAuthenticator.php
+++ app/src/src/Security/AppAuthenticator.php
@@ -55,7 +55,10 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
         // redirect to some "app_homepage" route - of wherever you want
-     return new RedirectResponse($this->urlGenerator->generate('app_cv_show'));
+     //return new RedirectResponse($this->urlGenerator->generate('app_cv_show'));
+     return new RedirectResponse($this->urlGenerator->generate('app_dashboard'));
+
+
     }
 
     protected function getLoginUrl(Request $request): string
